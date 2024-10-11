a691a6ece76b49c340daa9c6790b60cce2635e7a
NHATRUONG <tt912002@gmail.com>
2024-10-11 16:14:57 +0700

    Delete Server/.history directory

--- Server/.history/routes/courseRoutes_20241006224313.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createCourse, getCourses } = require("../controllers/courseController");
-const { verifyToken } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/create", verifyToken, createCourse);
-router.get("/", getCourses);
-
-module.exports = router;
--- Server/.history/src/routes/userRoutes_20241011130428.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { userController } from "~/controllers/userController";
-import { verifyToken } from "~/middleware/authMiddleware";
-
-const router = express.Router();
-
-router.get("/profile", verifyToken, userController.getUserProfile);
-
-export default router;

f4f4796f77e6113600e24ae626d4cc905aaf2bbe
NhaTruong <tt912002@gmail.com>
2024-10-15 21:24:10 +0700

    fixing lesson test

--- Server/src/controllers/course/chapterController.js
+++ Server/src/controllers/course/chapterController.js
@@ -1,5 +1,5 @@
-const message = require('~/config/message.json');
-const chapterService = require('~/services/course/chapterService');
+const message = require("~/config/message.json");
+const chapterService = require("~/services/course/chapterService");
 
 const sendResponse = (res, success, title, description = null) => {
   res.status(success ? 201 : 400).send({
@@ -10,7 +10,7 @@ const sendResponse = (res, success, title, description = null) => {
 };
 
 const chapterController = {
-     /**
+  /**
    * Xử lý yêu cầu tạo chapter mới cho khóa học.
    * @param {Object} req - Yêu cầu từ client.
    * @param {Object} res - Đối tượng response để gửi phản hồi về client.
@@ -29,13 +29,14 @@ const chapterController = {
     }
 
     try {
-      const result = await chapterService.createChapter(userID, courseID, chapterName);
-      sendResponse(
-        res,
-        true,
-        message.chapter.creationSuccess.title,
-        { chapterID: result }
+      const result = await chapterService.createChapter(
+        userID,
+        courseID,
+        chapterName
       );
+      sendResponse(res, true, message.chapter.creationSuccess.title, {
+        chapterID: result,
+      });
     } catch (err) {
       console.error(err); // Ghi lại lỗi để kiểm tra
 
@@ -48,39 +49,28 @@ const chapterController = {
     }
   },
 
-    updateChapterName: async (req, res) => {
-      const chapterID = req.body.chapterID;
-      const name = req.body.chapterName;
-      const userID = req.body.userID;
-    
-      // Kiểm tra các tham số đầu vào
-      if (!chapterID || !name || !userID) {
-        return sendResponse( 
-          res,
-          false,
-          message.chapter.updateError.title,
-          message.chapter.updateError.description.missRequireInfor
-        );
-      }
-    
-      try {
-        await chapterService.updateChapterName(userID, chapterID, name);
-        sendResponse(
-          res,
-          true,
-          message.chapter.updateSuccess.title,
-          null
-        );
-      } catch (err) {
-        sendResponse(
-          res,
-          false,
-          message.chapter.updateError.title,
-          err.message
-        );
-      }
+  updateChapterName: async (req, res) => {
+    const chapterID = req.body.chapterID;
+    const name = req.body.chapterName;
+    const userID = req.body.userID;
+
+    // Kiểm tra các tham số đầu vào
+    if (!chapterID || !name || !userID) {
+      return sendResponse(
+        res,
+        false,
+        message.chapter.updateError.title,
+        message.chapter.updateError.description.missRequireInfor
+      );
     }
-    
-}
+
+    try {
+      await chapterService.updateChapterName(userID, chapterID, name);
+      sendResponse(res, true, message.chapter.updateSuccess.title, null);
+    } catch (err) {
+      sendResponse(res, false, message.chapter.updateError.title, err.message);
+    }
+  },
+};
 
 module.exports = chapterController;
--- Server/src/routes/courseRoutes.js
+++ Server/src/routes/courseRoutes.js
@@ -1,21 +1,26 @@
 const express = require("express");
-const multer = require("multer");
 const courseController = require("../controllers/course/courseController");
 const chapterController = require("../controllers/course/chapterController");
-const videoController = require("../controllers/course/videoController");
-const courseValidator = require('../validation/courseValidation');
+const lessonController = require("../controllers/course/lessonController");
+const courseValidator = require("../validation/courseValidation");
 
 const router = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/create",courseValidator.createCourse, courseController.createCourse);
+// Routes liên quan đến course
+router.post(
+  "/create",
+  courseValidator.createCourse,
+  courseController.createCourse
+);
 router.post("/update/name", courseController.updateCourseName);
 
-// Chapter
+// Routes liên quan đến chapter
 router.post("/chapter/create", chapterController.create);
 router.post("/chapter/update/name", chapterController.updateChapterName);
 
-// Video 
-router.post("/video/create",upload.single("video"), videoController.uploadVideo)
+// Routes liên quan đến lesson
+router.post("/lesson/create", lessonController.create);
+router.post("/lesson/update", lessonController.updateLesson);
+router.post("/lesson/delete", lessonController.delete);
 
 module.exports = router;

2c6dd236f26b1dcbaef29fe68c44cad70f1fcd8d
lamnino08 <phanduclam02@gmail.com>
2024-11-02 10:58:45 +0700

    enrollment sql

--- Server/SQL files/elearning-enroll.sql
+++ Server/SQL files/elearning-enroll.sql
@@ -1,21 +1,38 @@
 CREATE TABLE enrollment (
   EnrollmentID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   UserID INT UNSIGNED NOT NULL,
-  CourseID INT UNSIGNED NOT NULL, -- Giả sử có thể liên kết với Course Service
-  FOREIGN KEY (UserID) REFERENCES user(UserID) ON DELETE CASCADE
+  CourseID INT UNSIGNED NOT NULL,
+  StartDate DATE DEFAULT CURRENT_DATE,             
+  EndDate DATE,                                     
+  Status ENUM('buying', 'cancelled', 'bought', 'learning', completed) DEFAULT 'buying', 
+  Rating INT CHECK(Rating BETWEEN 1 AND 5),        
+  Review TEXT,                                     
+  CompletionDate DATE,    
+  Cost: Float DEFAULT 0                         
 );
 
 CREATE TABLE progress (
   ProgressID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  EnrollmentID INT UNSIGNED NOT NULL,     -- ID của học sinh trong khóa học
-  LessonID INT UNSIGNED NOT NULL,         -- Bài học mà học sinh đang theo dõi
-  ProgressTime INT DEFAULT 0,             -- Thời gian dừng video hoặc phần trăm hoàn thành bài học
-  UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);  
+  EnrollmentID INT UNSIGNED NOT NULL,
+  LessonID INT UNSIGNED NOT NULL,
+  ProgressTime INT DEFAULT 0,  
+  IsCompleted BOOLEAN DEFAULT FALSE, 
+  CompletionDate DATETIME DEFAULT NULL, 
+  Attempts INT UNSIGNED DEFAULT 0, 
+  LastAccessedAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
+  FOREIGN KEY (EnrollmentID) REFERENCES enrollment(EnrollmentID) ON DELETE CASCADE,
+  FOREIGN KEY (LessonID) REFERENCES lesson(LessonID) ON DELETE CASCADE
+);
+
 
 CREATE TABLE note (
   NoteID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  ProgressID INT UNSIGNED NOT NULL, -- Giả sử có bảng progress bên ngoài để theo dõi tiến độ
-  Content TEXT DEFAULT NULL
+  ProgressID INT UNSIGNED NOT NULL,
+  Title VARCHAR(100) DEFAULT NULL,           
+  Content TEXT DEFAULT NULL,                 
+  atTime INT UNSIGNED DEFAULT 0,            
+  IsImportant BOOLEAN DEFAULT FALSE,         
+  CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
+  UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+  FOREIGN KEY (ProgressID) REFERENCES progress(ProgressID) ON DELETE CASCADE
 );
-

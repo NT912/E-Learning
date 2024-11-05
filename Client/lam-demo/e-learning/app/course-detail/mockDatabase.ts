28bf6958c6d636981678603619dd58fa9630947c
lamnino08 <phanduclam02@gmail.com>
2024-11-05 17:36:33 +0700

    [Server] init payment VNPAY

---
+++ Client/lam-demo/e-learning/app/course-detail/mockDatabase.ts
@@ -0,0 +1,8 @@
+import { Course } from './types';
+
+export const mockCourse: Course = {
+  id: 1,
+  title: 'Intro to Next.js',
+  description: 'Learn the basics of Next.js in this introductory course.',
+  price: 100000,
+};
